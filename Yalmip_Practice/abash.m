d1fb5d64e10675a4e2bd36b242f21a61b7e0737b
Alexander Frankel <frankeal@oregonstate.edu>
2026-04-10 19:27:57 -0700

    Saving progress

--- Yalmip_Practice/abash.m
+++ Yalmip_Practice/abash.m
@@ -1,16 +1,19 @@
 function [X] = abash(M, Data, Oper, MU, row, dt)
-if Data == 0
+
+if isequal(Data,[])
     if row <= 2
         X = (M(1,:)'+(Oper*(M(1,:)'))*dt)';
     else
         X = (M(row-1,:)'+(1.5*Oper*(M(row-1,:)')-0.5*Oper*(M(row-2,:)'))*dt)';
     end
-else
-    if row <= 2
-        X = (M(1,:)'+(Oper*(M(1,:)')-MU.*(M(1,:)'-Data(1,:)'))*dt)';
-    else
-        X = (M(row-1,:)'+(1.5*Oper*(M(row-1,:)')-0.5*Oper*(M(row-2,:)')-MU.*(M(row-1,:)'-Data(row-1,:)'))*dt)';
-    end
+return;
 end
+    
+if row <= 2
+    X = (M(1,:)'+(Oper*(M(1,:)')-MU.*(M(1,:)'-Data(1,:)'))*dt)';
+else
+    X = (M(row-1,:)'+(1.5*Oper*(M(row-1,:)')-0.5*Oper*(M(row-2,:)')-MU.*(M(row-1,:)'-Data(row-1,:)'))*dt)';
+end
+
 
 end
--- Yalmip_Practice/timevo.m
+++ Yalmip_Practice/timevo.m
@@ -1,16 +1,17 @@
 function [M] = timevo(Oper,data,x0,MU,steps)
 
 
-dt = 0.00001;
+dt = 0.0001;
 Alen = size(Oper,1);
 
 
-if data == 0
+if isequal(data,[])
 data = [x0;zeros(steps-1,Alen)];
 for i = 2:steps
-data(i,:) = abash(data,0,Oper,0,i,dt);
+data(i,:) = abash(data,[],Oper,[],i,dt);
 end
-
+    M = data;
+    return;
 end
 
 M = [x0;zeros(steps-1,Alen)];
